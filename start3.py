4bfe00d28417b890be387e4fbd62268c6eae6c37
alanhaziev324 <56869776+alanhaziev324@users.noreply.github.com>
2020-05-12 18:20:14 +0300

    Update start3.py

--- start3.py
+++ start3.py
@@ -1,23 +1,10 @@
 from collections import namedtuple
+
 import requests
+
 import flask_sqlalchemy
 
 from flask import Flask, render_template, redirect, url_for, request
-
-
-@app.route('/answer')
-@app.route('/auto_answer')
-def auto_answer():
-    profil = {}
-    profil['title'] = ''
-    profil['surname'] = ''
-    profil['name'] = ''
-    profil['education'] = ''
-    profil['profession'] = ''
-    profil['sex'] = ''
-    profil['motivation'] = ''
-    profil['ready'] = 'True'
-    return render_template('auto_answer.html', **profil)
 
 
 class MapParams(object):
@@ -45,30 +32,7 @@ def load_map(mp):
     # Запись полученного изображения в файл.
     map_file = "1.jpg"
 
-class LoginForm(FlaskForm):
-    userid = StringField('Id астронавта', validators=[DataRequired()])
-    password_1 = PasswordField('Пароль астронавта', validators=[DataRequired()])
-    cap_id = StringField('Id капитана', validators=[DataRequired()])
-    password_2 = PasswordField('Пароль капитана', validators=[DataRequired()])
-    submit = SubmitField('Доступ')
 
-
-@app.route('/login1', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        return redirect('/success')
-    return render_template('login.html', title='Аварийный доступ', form=form)
-    def main():
-    global_init(input())
-
-    session = create_session()
-
-    for job in session.query(Jobs).filter((Jobs.work_size < 20),
-                                          Jobs.is_finished == 0):
-        print(job)
-    
-    
 app = Flask(__name__)
 
 Message = namedtuple('Message', 'text tag')
@@ -90,6 +54,119 @@ def main1():
     return '''<img src="/static/img/1.jpg" alt="здесь должна была быть картинка, но не нашлась">'''
 
 
+@app.route('/otzivi1', methods=['GET'])
+def main5():
+    return '''<img src="/static/img/2.jpg" alt="здесь должна была быть картинка, но не нашлась">'''
+
+
+@app.route('/otzivi11', methods=['GET'])
+def main55():
+    return '''<img src="/static/img/3.jpg" alt="здесь должна была быть картинка, но не нашлась">'''
+
+
+@app.route('/astronaut_selection', methods=['POST', 'GET'])
+def astronaut_selection():
+    if request.method == 'GET':
+        return f'''<!doctype html>
+                        <html lang="en">
+                          <head>
+                            <meta charset="utf-8">
+                            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+                            <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                            <title>Отбор астронавтов</title>
+                          </head>
+                          <body>
+                            <h1 align="center">Анкета претендента</h1>
+                            <h3 align="center">на участие в миссии</h3>
+                            <div>
+                                <form class="login_form" method="post">
+                                    <input type="text" class="form-control" id="surname" aria-describedby="surnamelHelp" placeholder="Введите фамилию" name="surname">
+
+                                    <input type="text" class="form-control" id="name" aria-describedby="nameHelp" placeholder="Введите имя" name="name">
+                                    <br>
+                                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Введите адрес почты" name="email">
+                                    <div class="form-group">
+                                        <label for="eduSelect">Какое у Вас образование?</label>
+                                        <select class="form-control" id="classSelect" name="edu">
+                                          <option>Начальное</option>
+                                          <option>Среднее</option>
+                                          <option>Выше среднего</option>
+                                          <option>Супер!</option>
+                                        </select>
+                                     </div>
+                                        <label for="eduSelect">Какие у Вас есть профессии?</label>
+                                    <div class="form-group form-check">
+                                        <input type="checkbox" class="form-check-input" id="prof" name="prof">
+                                        <label class="form-check-label" for="acceptRules">Инженер-исследователь</label>
+                                        <br><input type="checkbox" class="form-check-input" id="prof" name="prof1">
+                                        <label class="form-check-label" for="acceptRules">Инженер-строитель</label>
+                                        <br><input type="checkbox" class="form-check-input" id="prof" name="prof2">
+                                        <label class="form-check-label" for="acceptRules">Пилот</label>
+                                        <br><input type="checkbox" class="form-check-input" id="prof" name="prof3">
+                                        <label class="form-check-label" for="acceptRules">Метеоролог</label>
+                                        <br><input type="checkbox" class="form-check-input" id="prof" name="prof4">
+                                        <label class="form-check-label" for="acceptRules">Инженер по жизнеобеспечению</label>
+                                        <br><input type="checkbox" class="form-check-input" id="prof" name="prof5">
+                                        <label class="form-check-label" for="acceptRules">Инженер по радиационной защите</label>
+                                        <br><input type="checkbox" class="form-check-input" id="prof" name="prof6">
+                                        <label class="form-check-label" for="acceptRules">Врач</label>
+                                        <br><input type="checkbox" class="form-check-input" id="prof" name="prof7">
+                                        <label class="form-check-label" for="acceptRules">Экзобиолог</label>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <label for="form-check">Укажите пол</label>
+                                        <div class="form-check">
+                                          <input class="form-check-input" type="radio" name="sex" id="male" value="male" checked>
+                                          <label class="form-check-label" for="male">
+                                            Мужской
+                                          </label>
+                                        </div>
+                                        <div class="form-check">
+                                          <input class="form-check-input" type="radio" name="sex" id="female" value="female">
+                                          <label class="form-check-label" for="female">
+                                            Женский
+                                          </label>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="about">Почему Вы хотите принять участие в миссии?</label>
+                                        <textarea class="form-control" id="about" rows="3" name="about"></textarea>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="photo">Приложите фотографию</label>
+                                        <input type="file" class="form-control-file" id="photo" name="file">
+                                    </div>
+                                    <div class="form-group form-check">
+                                        <input type="checkbox" class="form-check-input" id="acceptRules" name="accept">
+                                        <label class="form-check-label" for="acceptRules">Готовы остаться на Марсе?</label>
+                                    </div>
+                                    <button type="submit" class="btn btn-primary">Отправить</button>
+                                </form>
+                            </div>
+                          </body>
+                        </html>'''
+    elif request.method == 'POST':
+        print(request.form['surname'])
+        print(request.form['name'])
+        print(request.form['email'])
+        print(request.form['edu'])
+        print(request.form['file'])
+        print(request.form['about'])
+        print(request.form['accept'])
+        print(request.form['sex'])
+        print(request.form['prof'])
+        print(request.form['prof1'])
+        print(request.form['prof2'])
+        print(request.form['prof3'])
+        print(request.form['prof4'])
+        print(request.form['prof5'])
+        print(request.form['prof6'])
+        print(request.form['prof7'])
+        return "Форма отправлена"
+
+
 @app.route('/add_message', methods=['POST'])
 def add_message():
     text = request.form['text']
@@ -102,3 +179,4 @@ def add_message():
 
 if __name__ == '__main__':
     app.run()
+
