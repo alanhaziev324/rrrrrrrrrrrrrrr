15cfc8054f4e186d3cc7f68299a40e054b4c30a5
alanhaziev324 <56869776+alanhaziev324@users.noreply.github.com>
2020-05-12 08:03:33 +0300

    Add files via upload

---
+++ start3.py
@@ -0,0 +1,66 @@
+from collections import namedtuple
+import requests
+import flask_sqlalchemy
+
+from flask import Flask, render_template, redirect, url_for, request
+
+
+class MapParams(object):
+    def __init__(self):
+        self.lat = 61.665279
+        self.lon = 50.813492
+        self.zoom = 16  # Масштаб карты на старте. Изменяется от 1 до 19
+        self.type = "map"  # Другие значения "sat", "sat,skl"
+
+    # Преобразование координат в параметр ll, требуется без пробелов, через запятую и без скобок
+    def ll(self):
+        return str(self.lon) + "," + str(self.lat)
+
+
+# Создание карты с соответствующими параметрами.
+def load_map(mp):
+    map_request = "http://static-maps.yandex.ru/1.x/?ll={ll}&z={z}&l={type}".format(ll=mp.ll(), z=mp.zoom, type=mp.type)
+    response = requests.get(map_request)
+    if not response:
+        print("Ошибка выполнения запроса:")
+        print(map_request)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        sys.exit(1)
+
+    # Запись полученного изображения в файл.
+    map_file = "1.jpg"
+
+
+app = Flask(__name__)
+
+Message = namedtuple('Message', 'text tag')
+messages = []
+
+
+@app.route('/', methods=['GET'])
+def hello_world():
+    return render_template('index.html')
+
+
+@app.route('/login', methods=['GET'])
+def main():
+    return render_template('main.html', messages=messages)
+
+
+@app.route('/otzivi', methods=['GET'])
+def main1():
+    return '''<img src="/static/img/1.jpg" alt="здесь должна была быть картинка, но не нашлась">'''
+
+
+@app.route('/add_message', methods=['POST'])
+def add_message():
+    text = request.form['text']
+    tag = request.form['tag']
+
+    messages.append(Message(text, tag))
+
+    return redirect(url_for('main'))
+
+
+if __name__ == '__main__':
+    app.run()
